ff431cc2857918ccce9de83614c6d8a7e15aa93f
mizutamasato <117154853+mizutamasato@users.noreply.github.com>
2022-12-24 04:22:08 +0000

    Finishgit add -A

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   #get 'users/edit'
   #get 'users/show'
   #get 'users/index'
-  
+
   #get 'books/new'
   #get 'books/index'
   #get 'books/show'
@@ -12,9 +12,11 @@ Rails.application.routes.draw do
   resources :books, only: [:create, :index, :show, :edit, :update, :destroy ]
 
   resources :users, only: [:index, :show, :edit, :update ]
-  
+
   root to: "homes#top"
-  
-  get 'homes/about'
+
+  get "home/about" => "homes#about" ,as: "about"
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
